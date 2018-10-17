f70b9a646e1467807c41f7bf1072c72bc4a9a403
徐灿辉 <xucanhui@4399inc.com>
2018-10-17 16:04:29 +0800

    handle video thumb edit

--- app/src/main/java/com/m4399/videoeditor/ui/EditThumbActivity.java
+++ app/src/main/java/com/m4399/videoeditor/ui/EditThumbActivity.java
@@ -4,11 +4,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.SeekBar;
 
+import com.kk.taurus.playerbase.entity.DataSource;
+import com.kk.taurus.playerbase.event.OnPlayerEventListener;
+import com.kk.taurus.playerbase.render.IRender;
+import com.kk.taurus.playerbase.widget.BaseVideoView;
 import com.m4399.videoeditor.R;
+import com.m4399.videoeditor.widget.VideoThumbProgressBar;
 
-public class EditThumbActivity extends AppCompatActivity
+public class EditThumbActivity extends AppCompatActivity implements SeekBar.OnSeekBarChangeListener,
+                                                                    OnPlayerEventListener
 {
+    private static final String TAG = "EditThumbActivity";
+
+    private BaseVideoView mVideoView;
+    private VideoThumbProgressBar mVideoThumbProgressBar;
+
+    private long mTotalTime;
+
+    private String mVideoPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -17,6 +33,20 @@ public class EditThumbActivity extends AppCompatActivity
         setContentView(R.layout.activity_edit_thumb);
 
         setTitle("编辑封面");
+
+        mVideoPath = getIntent().getStringExtra("video_path");
+
+        mVideoView = findViewById(R.id.video_view);
+        mVideoView.setOnPlayerEventListener(this);
+        mVideoView.setDataSource(new DataSource(mVideoPath));
+        mVideoView.setRenderType(IRender.RENDER_TYPE_TEXTURE_VIEW);
+        mVideoView.start();
+
+        mVideoThumbProgressBar = findViewById(R.id.video_range_slider);
+        mVideoThumbProgressBar.setOnSeekBarChangeListener(this);
+        mVideoThumbProgressBar.setDataSource(mVideoPath);
+
+        mTotalTime = mVideoThumbProgressBar.getTotalTime();
     }
 
     public static void open(Context context, String videoPath)
@@ -24,5 +54,42 @@ public class EditThumbActivity extends AppCompatActivity
         Intent intent = new Intent(context, EditThumbActivity.class);
         intent.putExtra("video_path", videoPath);
         context.startActivity(intent);
+    }
+
+    @Override
+    public void onPlayerEvent(int eventCode, Bundle bundle)
+    {
+        switch (eventCode)
+        {
+            case PLAYER_EVENT_ON_START:
+                mVideoView.pause();
+                break;
+        }
+    }
+
+    @Override
+    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser)
+    {
+        float percent = (float)progress / 1000;
+        Log.i(TAG, "progress:" + progress + " percent:" + percent);
+
+        progress = (int) (mTotalTime * percent);
+
+        if (mVideoView != null)
+        {
+            mVideoView.seekTo(progress);
+        }
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar)
+    {
+
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar)
+    {
+
     }
 }
---
+++ app/src/main/java/com/m4399/videoeditor/widget/VideoThumbProgressBar.java
@@ -0,0 +1,86 @@
+package com.m4399.videoeditor.widget;
+
+
+import android.content.Context;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.widget.FrameLayout;
+import android.widget.SeekBar;
+
+import com.m4399.videoeditor.R;
+import com.m4399.videoeditor.adapter.VideoThumbnailAdapter;
+import com.m4399.videoeditor.media.FrameExtractor;
+
+public class VideoThumbProgressBar extends FrameLayout
+{
+    private RecyclerView mVideoThumbnailGallery;
+    private SeekBar mSeekBar;
+
+    private VideoThumbnailAdapter mThumbnailAdapter;
+
+    private FrameExtractor mFrameExtractor;
+
+    private long mTotalTime;
+
+    public VideoThumbProgressBar(Context context)
+    {
+        super(context);
+
+        initView();
+    }
+
+    public VideoThumbProgressBar(Context context, AttributeSet attrs)
+    {
+        super(context, attrs);
+
+        initView();
+    }
+
+    public VideoThumbProgressBar(Context context, AttributeSet attrs, int defStyleAttr)
+    {
+        super(context, attrs, defStyleAttr);
+
+        initView();
+    }
+
+    private void initView()
+    {
+        LayoutInflater.from(getContext()).inflate(R.layout.video_thumb_progress_bar, this);
+
+        mFrameExtractor = new FrameExtractor();
+
+        mVideoThumbnailGallery = findViewById(R.id.video_thumbnails);
+        mVideoThumbnailGallery.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        mThumbnailAdapter = new VideoThumbnailAdapter(getContext(), mFrameExtractor);
+        mVideoThumbnailGallery.setAdapter(mThumbnailAdapter);
+
+        mSeekBar = findViewById(R.id.seek_bar);
+        mSeekBar.setMax(1000);
+        mSeekBar.setPadding(0, 0, 0 , 0);
+    }
+
+    public void setFrameProgress(float percent)
+    {
+        mSeekBar.setProgress((int) percent);
+    }
+
+    public void setDataSource(String path)
+    {
+        mFrameExtractor.setDataSource(path);
+
+        mTotalTime = mThumbnailAdapter.fetchDuration();
+    }
+
+    public long getTotalTime()
+    {
+        return mTotalTime;
+    }
+
+
+    public void setOnSeekBarChangeListener(SeekBar.OnSeekBarChangeListener listener)
+    {
+        mSeekBar.setOnSeekBarChangeListener(listener);
+    }
+}
